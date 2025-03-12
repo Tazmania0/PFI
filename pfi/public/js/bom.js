788f5d97cbe4c5cb71ecec8e5fc9eebd166eafca
tazmania0 <tzmail2000@gmail.com>
2025-03-12 15:51:05 +0200

    bom fix

--- pfi/public/js/bom.js
+++ pfi/public/js/bom.js
@@ -1,9 +1,17 @@
 // bom.js
 frappe.ui.form.on('BOM', {
     refresh: function(frm) {
-        frm.toggle_display(['operations'], !frm.doc.is_service_bom);
+        frm.toggle_reqd('items', !frm.doc.is_service_bom);
+        frm.toggle_display(['items', 'operations'], !frm.doc.is_service_bom);
     },
-    _is_service: function(frm) {
-        frm.toggle_display(['operations'], !frm.doc._is_service_bom);
+    is_service_bom: function(frm) {
+        frm.toggle_reqd('items', !frm.doc.is_service_bom);
+        frm.toggle_display(['items', 'operations'], !frm.doc.is_service_bom);
+        
+        // Clear items table if "Is Service" is checked
+        if (frm.doc.is_service_bom) {
+            frm.clear_table('items');
+            frm.refresh_field('items');
+        }
     }
 });
