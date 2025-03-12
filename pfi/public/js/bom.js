647fd50d84bdd4304af9b383732cc0a6e2fb6036
tazmania0 <tzmail2000@gmail.com>
2025-03-12 16:01:46 +0200

    bom fix

--- pfi/public/js/bom.js
+++ pfi/public/js/bom.js
@@ -1,14 +1,20 @@
-// bom.js
 frappe.ui.form.on('BOM', {
     refresh: function(frm) {
+        // Toggle Items table visibility/mandatory based on "_is_service"
         frm.toggle_reqd('items', !frm.doc.is_service_bom);
-        frm.toggle_display(['items', 'operations'], !frm.doc.is_service_bom);
+        frm.toggle_display('items', !frm.doc.is_service_bom);
+        
+        // Clear Items table if "Is Service" is checked
+        if (frm.doc.is_service_bom) {
+            frm.clear_table('items');
+            frm.refresh_field('items');
+        }
     },
     is_service_bom: function(frm) {
+        // Update on checkbox change
         frm.toggle_reqd('items', !frm.doc.is_service_bom);
-        frm.toggle_display(['items', 'operations'], !frm.doc.is_service_bom);
+        frm.toggle_display('items', !frm.doc.is_service_bom);
         
-        // Clear items table if "Is Service" is checked
         if (frm.doc.is_service_bom) {
             frm.clear_table('items');
             frm.refresh_field('items');
