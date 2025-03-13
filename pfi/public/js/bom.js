f2d1445ab27fd3e2b0de384ace2472105e2474a9
tazmania0 <tzmail2000@gmail.com>
2025-03-13 10:48:02 +0200

    fix typo

--- pfi/public/js/bom.js
+++ pfi/public/js/bom.js
@@ -2,7 +2,7 @@
 frappe.ui.form.on('BOM', {
     onload: function(frm) {
         // Initial setup: Hide "is_service_bom" if "With Operations" is unchecked
-        frm.toggle_display('_is_service', frm.doc.with_operations);
+        frm.toggle_display('is_service_bom', frm.doc.with_operations);
     },
     refresh: function(frm) {
         // Toggle Items visibility/requirement
@@ -16,7 +16,7 @@ frappe.ui.form.on('BOM', {
         // Show/hide "is_service_bom" based on "With Operations"
         frm.toggle_display('is_service_bom', frm.doc.with_operations);
         
-        // Uncheck "_is_service" if "With Operations" is unchecked
+        // Uncheck "is_service_bom" if "With Operations" is unchecked
         if (!frm.doc.with_operations && frm.doc.is_service_bom) {
             frm.set_value('is_service_bom', 0);
         }
