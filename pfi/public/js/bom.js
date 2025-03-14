e9ff6ab14c8172abaea8677d4526c7ea0eea0658
tazmania0 <tzmail2000@gmail.com>
2025-03-14 13:55:27 +0200

    fix

--- pfi/public/js/bom.js
+++ pfi/public/js/bom.js
@@ -26,4 +26,43 @@ frappe.ui.form.on('BOM', {
             frm.refresh_field('items');
         }
     }
+	
+    // Override client-side mandatory check
+    before_save: function(frm) {
+        if (frm.doc.is_service_bom) {
+            // Bypass items check for service BOMs
+            frm.doc.items = [];
+            frm.refresh_field('items');
+        }
+    },
+
+    // UI Handling
+    refresh: function(frm) {
+        frm.toggle_display('is_service_bom', frm.doc.with_operations);
+        frm.toggle_display(['items', 'scrap_items'], !frm.doc.is_service_bom);
+        
+        if (frm.doc.is_service_bom) {
+            frm.clear_table('items');
+            frm.clear_table('scrap_items');
+            frm.refresh_field('items');
+            frm.refresh_field('scrap_items');
+        }
+    },
+
+    with_operations: function(frm) {
+        frm.toggle_display('is_service_bom', frm.doc.with_operations);
+        if (!frm.doc.with_operations && frm.doc.is_service_bom) {
+            frm.set_value('is_service_bom', 0);
+        }
+    },
+
+    is_service_bom: function(frm) {
+        frm.toggle_display(['items', 'scrap_items'], !frm.doc.is_service_bom);
+        if (frm.doc.is_service_bom) {
+            frm.clear_table('items');
+            frm.clear_table('scrap_items');
+            frm.refresh_field('items');
+            frm.refresh_field('scrap_items');
+        }
+    }
 });
